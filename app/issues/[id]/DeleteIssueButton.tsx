3d674983e5cce932c3e79692600777daed54744c
NaVeEn-Kurma <nk5442024@gmail.com>
2023-12-13 11:36:08 +0530

    Improved UI by adding Spinner

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Spinner } from '@/app/components'
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
@@ -8,13 +9,16 @@ import { useState } from 'react'
 const DeleteIssueButton = ({IssueId}:{IssueId:number}) => {
     const router=useRouter();
     const [error,setError]=useState(false);
+    const [isDeleting,setIsdeleting]=useState(false);
 
     const deleteIssue=async()=>{
         try {
+            setIsdeleting(true);
             await axios.delete(`/api/issues/${IssueId}`)
             router.push('/issues')
             router.refresh()
         } catch (error) {
+            setIsdeleting(false);
             setError(true);
         }
     }
@@ -23,8 +27,9 @@ const DeleteIssueButton = ({IssueId}:{IssueId:number}) => {
     <>
     <AlertDialog.Root>
         <AlertDialog.Trigger>
-            <Button color='red'>
+            <Button color='red' disabled={isDeleting}>
                 Delete Issue
+                {isDeleting && <Spinner/>}
             </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content >
